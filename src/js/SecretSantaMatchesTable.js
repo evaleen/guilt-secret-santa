fbecf7bf5db54747bcf9f84a78705b073270df14
Eibhlin McGeady <eibhlinmcg93@gmail.com>
2017-05-08 16:04:13 +0100

    display basic version of matches

--- src/js/SecretSantaMatchesTable.js
+++ src/js/SecretSantaMatchesTable.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 import '../css/SecretSantaMatchesTable.css';
 
+import { findWhere } from 'underscore';
+
 class SecretSantaMatchesTable extends Component {
 
-  render() {
+  renderMatches() {
     const { participants, assignedSecretSantas } = this.props;
+    const matchesList = [];
+    participants.forEach((participant, index) => {
+      const match = findWhere(assignedSecretSantas, { giver: participant.guid });
+      const receiver = findWhere(participants, { guid: match.receiver });
+      console.log(receiver);
+      matchesList.push(
+        <div
+          key={index}
+        >
+          <p>Gifter: {participant.name.first} {participant.name.last}</p>
+          <p>Receiver: {receiver.name.first} {receiver.name.last}</p>
+        </div>
+      );
+    });
+    return (
+      <div>
+        {matchesList}
+      </div>
+    );
+  }
+
+  render() {
+    const matches = this.renderMatches();
     return (
       <div className="matches-table">
+        {matches}
       </div>
     );
   }
